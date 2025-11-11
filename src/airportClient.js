32e74ff0c148d4e8bb458b458109b3b4fa8d617a
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-11-11 21:19:11 +0100

    docs: förtydliga skillnaden mellan låst och senaste gist-URL för airports.json

--- src/airportClient.js
+++ src/airportClient.js
@@ -1,5 +1,9 @@
 const REMOTE_URL =
-  'https://gist.githubusercontent.com/eriobe/9e86b6e1cf69b52c6cad59963fdc2624/raw/3e25475edd3084bd93233ee80710a87a1dddb582/airports.json';
+  /* Låst, verifierad version (med commit-hash): 'https://gist.githubusercontent.com/eriobe/9e86b6e1cf69b52c6cad59963fdc2624/raw/3e25475edd3084bd93233ee80710a87a1dddb582/airports.json'; */
+  // Alltid senaste versionen (så länge aktuell gist finns kvar):
+  'https://gist.githubusercontent.com/eriobe/9e86b6e1cf69b52c6cad59963fdc2624/raw/airports.json';
+
+// Fallback lösning (ovanstående commit-hash version har dessutom sparats lokalt):
 const LOCAL_URL = 'airports.json';
 
 async function getAirports(url) {
